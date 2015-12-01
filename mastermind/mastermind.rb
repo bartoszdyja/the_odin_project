6edf70e4025e9eb47c56cdf09c00d94d3ac754a4
railsbeginner <bartosz.dyja@gmail.com>
2015-12-01 15:50:04 +0100

    completed mastermind

--- mastermind/mastermind.rb
+++ mastermind/mastermind.rb
@@ -1,13 +1,45 @@
 class Game
+  COLORS = ['R','G','B', 'O', 'D']
   def initialize
     @player = Player.new('Tom')
+    @results = []
+    @answer = randomize()
+  end
+
+  def randomize
+   results = []
+   4.times {results<<COLORS.sample}
+   results
   end
 
   def play
-    current_guess=@player.guess_code
     10.times do |i|
-      puts current_guess
+      puts
+      puts "Attempt no: #{i+1}"
+      current_guess=@player.guess_code
+      return if current_guess == 'quit'
+      @results << [current_guess, tip(current_guess)]
+      show_results
+      if tip(current_guess)[1]==4
+        puts 'You won!!!'
+        return
+      end
     end
+    puts "The combination was #{@answer}"
+  end
+
+  def tip (guess)
+    near = []
+    exact = []
+    @answer.each_with_index do |l,i|
+      near << l if guess.include?(l)
+      exact << l if guess[i]==l
+    end
+    [near.length,exact.length]
+  end
+
+  def show_results
+    @results.each_with_index {|x,y| puts "#{y+1}: Combination #{x[0]} has #{x[1][1]} exact maches and #{x[1][0]-x[1][1]} near matches. "}
   end
 end
 
@@ -22,11 +54,12 @@ class Player
   end
 
   def validate_answer answer
-    if /^([ABCD]){4}\b/=~answer
+    if /^([RGBOD]){4}\b|^quit\b/=~answer.upcase!
       return answer
     else
       puts 'I don\'t understand your answer.'
-      puts 'Please make a guess in format XXXX'
+      puts 'Please make a guess in format XXXX or type \'quit\''
+      puts
       guess_code
     end
   end
